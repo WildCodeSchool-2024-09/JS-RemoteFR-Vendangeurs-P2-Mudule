e7517d8340f5f7a9f74163eb5f8686f0db112aa0
Foxrim <Flavien085@gmail.com>
2024-11-18 15:48:48 +0100

    [fix] - Code répétitif remplacé par des boucles

--- client/src/components/DeskFooter.tsx
+++ client/src/components/DeskFooter.tsx
@@ -12,107 +12,65 @@ import water_shadow from "../../public/water_shadow.webp";
 import styles from "../styles/DeskFooter.module.css";
 
 export default function DeskFooter() {
+  const trees = [];
+  const bushes = [];
+  const fences_verticales = [];
+  const fences_horizontales = [];
+  const lac_verticales = [];
+
+  for (let i = 1; i <= 16; i++) {
+    trees.push(<img className={styles[`tree_${i}`]} src={tree} alt="arbre" />);
+  }
+
+  for (let i = 1; i <= 22; i++) {
+    bushes.push(
+      <img className={styles[`bush_${i}`]} src={bush} alt="buisson" />,
+    );
+  }
+
+  for (let i = 1; i <= 4; i++) {
+    fences_verticales.push(
+      <img
+        className={styles[`fence_vertical_${i}`]}
+        src={fence_vertical}
+        alt="barrière vertical"
+      />,
+    );
+  }
+
+  for (let i = 1; i <= 4; i++) {
+    fences_horizontales.push(
+      <img
+        className={styles[`fence_horizontal_${i}`]}
+        src={fence_horizontal}
+        alt="barrière horizontal"
+      />,
+    );
+  }
+
+  for (let i = 1; i <= 3; i++) {
+    lac_verticales.push(
+      <img
+        className={styles[`lac_vertical_${i}`]}
+        src={lac_vertical}
+        alt="bord de lac"
+      />,
+    );
+  }
+
   return (
     <footer className={styles.footer}>
+      {trees}
+      {bushes}
+      {fences_verticales}
+      {fences_horizontales}
+      {lac_verticales}
       <img className={styles.road_1} src={road} alt="chemin de terre" />
       <img className={styles.road_2} src={road} alt="chemin de terre" />
-      <img
-        className={styles.fence_vertical_1}
-        src={fence_vertical}
-        alt="barrière vertical"
-      />
-      <img
-        className={styles.fence_vertical_2}
-        src={fence_vertical}
-        alt="barrière vertical"
-      />
-      <img
-        className={styles.fence_vertical_3}
-        src={fence_vertical}
-        alt="barrière vertical"
-      />
-      <img
-        className={styles.fence_vertical_4}
-        src={fence_vertical}
-        alt="barrière vertical"
-      />
-      <img
-        className={styles.fence_horizontal_1}
-        src={fence_horizontal}
-        alt="barrière horizontal"
-      />
-      <img
-        className={styles.fence_horizontal_2}
-        src={fence_horizontal}
-        alt="barrière horizontal"
-      />
-      <img
-        className={styles.fence_horizontal_3}
-        src={fence_horizontal}
-        alt="barrière horizontal"
-      />
-      <img
-        className={styles.fence_horizontal_4}
-        src={fence_horizontal}
-        alt="barrière horizontal"
-      />
       <img
         className={styles.fence_corner}
         src={fence_corner}
         alt="coin de barrière"
-      />
-      <img className={styles.tree_1} src={tree} alt="arbre" />
-      <img className={styles.tree_2} src={tree} alt="arbre" />
-      <img className={styles.tree_3} src={tree} alt="arbre" />
-      <img className={styles.tree_4} src={tree} alt="arbre" />
-      <img className={styles.tree_5} src={tree} alt="arbre" />
-      <img className={styles.tree_6} src={tree} alt="arbre" />
-      <img className={styles.tree_7} src={tree} alt="arbre" />
-      <img className={styles.tree_8} src={tree} alt="arbre" />
-      <img className={styles.tree_9} src={tree} alt="arbre" />
-      <img className={styles.tree_10} src={tree} alt="arbre" />
-      <img className={styles.tree_11} src={tree} alt="arbre" />
-      <img className={styles.tree_12} src={tree} alt="arbre" />
-      <img className={styles.tree_13} src={tree} alt="arbre" />
-      <img className={styles.tree_14} src={tree} alt="arbre" />
-      <img className={styles.tree_15} src={tree} alt="arbre" />
-      <img className={styles.tree_16} src={tree} alt="arbre" />
-      <img className={styles.bush_1} src={bush} alt="buisson" />
-      <img className={styles.bush_2} src={bush} alt="buisson" />
-      <img className={styles.bush_3} src={bush} alt="buisson" />
-      <img className={styles.bush_4} src={bush} alt="buisson" />
-      <img className={styles.bush_5} src={bush} alt="buisson" />
-      <img className={styles.bush_6} src={bush} alt="buisson" />
-      <img className={styles.bush_7} src={bush} alt="buisson" />
-      <img className={styles.bush_8} src={bush} alt="buisson" />
-      <img className={styles.bush_9} src={bush} alt="buisson" />
-      <img className={styles.bush_10} src={bush} alt="buisson" />
-      <img className={styles.bush_11} src={bush} alt="buisson" />
-      <img className={styles.bush_12} src={bush} alt="buisson" />
-      <img className={styles.bush_13} src={bush} alt="buisson" />
-      <img className={styles.bush_14} src={bush} alt="buisson" />
-      <img className={styles.bush_15} src={bush} alt="buisson" />
-      <img className={styles.bush_16} src={bush} alt="buisson" />
-      <img className={styles.bush_17} src={bush} alt="buisson" />
-      <img className={styles.bush_18} src={bush} alt="buisson" />
-      <img className={styles.bush_19} src={bush} alt="buisson" />
-      <img className={styles.bush_20} src={bush} alt="buisson" />
-      <img className={styles.bush_21} src={bush} alt="buisson" />
-      <img className={styles.bush_22} src={bush} alt="buisson" />
-      <img
-        className={styles.lac_vertical_1}
-        src={lac_vertical}
-        alt="bord de lac"
-      />
-      <img
-        className={styles.lac_vertical_2}
-        src={lac_vertical}
-        alt="bord de lac"
-      />
-      <img
-        className={styles.lac_vertical_3}
-        src={lac_vertical}
-        alt="bord de lac"
       />
       <img className={styles.lac_corner} src={lac_corner} alt="coin du lac" />
       <img
