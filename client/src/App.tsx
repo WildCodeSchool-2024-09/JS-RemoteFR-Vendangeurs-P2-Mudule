42b8418b1f7d77bbd10afe29ce38c3027c3d2378
Foxrim <Flavien085@gmail.com>
2024-11-20 17:10:21 +0100

    [feat] - sound homepage & musique lancement & toggle version desktop

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import type React from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Homepage from "./components/Homepage";
 import "./App.css";
 import DeskPokedex from "./components/DeskPokedex";
@@ -12,9 +12,15 @@ const App: React.FC = () => {
   const [showHomepage, setShowHomepage] = useState(true);
   const [showFadeOut, setShowFadeOut] = useState(false);
   const [showFadeIn, setShowFadeIn] = useState(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
 
   const handlePokeballClick = () => {
     setShowFadeOut(true);
+    if (audioRef.current) {
+      audioRef.current.play().catch((error) => {
+        console.error("Error playing audio:", error);
+      });
+    }
     setTimeout(() => {
       setShowHomepage(false);
       setShowFadeOut(false);
@@ -34,6 +40,14 @@ const App: React.FC = () => {
   return (
     <>
       <div>
+        <audio ref={audioRef} src="./opening.mp3">
+          <track
+            kind="captions"
+            src="/captions.vtt"
+            srcLang="en"
+            label="English captions"
+          />
+        </audio>
         {showHomepage ? (
           <>
             <Homepage onPokeballClick={handlePokeballClick} />
--- client/src/components/DeskSound.tsx
+++ client/src/components/DeskSound.tsx
@@ -1,20 +1,65 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/DeskSound.module.css";
 
 export default function DeskSound() {
+  const [on, setOn] = useState<boolean>(false);
+  const audioRef = useRef<HTMLAudioElement>(null);
+
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = 0.14;
+    }
+  });
+
+  const toggleOn = () => {
+    if (audioRef.current) {
+      audioRef.current.play().catch((error) => {
+        console.error("Error playing audio:", error);
+      });
+      setOn(true);
+    }
+  };
+
+  const toggleOff = () => {
+    if (audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0;
+      setOn(false);
+    }
+  };
+
   return (
     <section className={styles.deskSound}>
+      {on && (
+        <audio ref={audioRef} src="./Gen2_intro.mp3" autoPlay>
+          <track
+            kind="captions"
+            src="/captions.vtt"
+            srcLang="en"
+            label="English captions"
+          />
+        </audio>
+      )}
       <div className={styles.blackBars}>
         <hr />
         <hr />
       </div>
       <div className={styles.soundButtonsContainer}>
         <div className={styles.soundButtons}>
-          <span className={styles.buttonShadowOn}>
+          <span
+            className={styles.buttonShadowOn}
+            onClick={toggleOn}
+            onKeyDown={(e) => e.key === "Enter" && toggleOn()}
+          >
             <span>
               <img src="#" alt="#" />
             </span>
           </span>
-          <span className={styles.buttonShadowOff}>
+          <span
+            className={styles.buttonShadowOff}
+            onClick={toggleOff}
+            onKeyDown={(e) => e.key === "Enter" && toggleOff()}
+          >
             <span>
               <img src="#" alt="#" />
             </span>
