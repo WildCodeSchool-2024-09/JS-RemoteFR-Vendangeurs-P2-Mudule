cea7588b54d86682e7622f574fab8e224ade082a
Foxrim <Flavien085@gmail.com>
2024-11-24 18:33:10 +0100

    [fix] - Reflet, animation menu burger, nuages, name pokémon

--- client/src/components/DeskHeader.tsx
+++ client/src/components/DeskHeader.tsx
@@ -4,6 +4,14 @@ import cloudOne from "../../public/cloud_1.webp";
 import styles from "../styles/DeskHeader.module.css";
 
 export default function DeskHeader() {
+  const clouds = [];
+
+  for (let i = 1; i <= 6; i++) {
+    clouds.push(
+      <img src={cloudOne} className={styles[`cloud_${i}`]} alt="nuage" />,
+    );
+  }
+
   return (
     <header className={styles.header}>
       <div>
@@ -14,11 +22,7 @@ export default function DeskHeader() {
         src={Ho_oh}
         alt="Ho-oh vole dans le ciel !"
       />
-      <img className={styles.cloudOne} src={cloudOne} alt="nuage" />
-      <img className={styles.cloudTwo} src={cloudOne} alt="nuage" />
-      <img className={styles.cloudThree} src={cloudOne} alt="nuage" />
-      <img className={styles.cloudFour} src={cloudOne} alt="nuage" />
-      <img className={styles.cloudFive} src={cloudOne} alt="nuage" />
+      {clouds}
       <span className={styles.sky} />
     </header>
   );
