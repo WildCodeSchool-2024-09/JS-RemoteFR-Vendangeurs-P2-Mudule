5577d6fbb6c8ff445defcc1ef502210306974646
Foxrim <Flavien085@gmail.com>
2024-11-25 16:58:14 +0100

    [feat] - animation buissons aléatoire

--- client/src/components/DeskFooter.tsx
+++ client/src/components/DeskFooter.tsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useState } from "react";
 import bush from "../../public/bush.webp";
 import fence_corner from "../../public/fence_corner.webp";
 import fence_horizontal from "../../public/fence_horizontal.webp";
@@ -10,11 +11,41 @@ import water_shadow from "../../public/water_shadow.webp";
 import styles from "../styles/DeskFooter.module.css";
 
 export default function DeskFooter() {
+  const [bush_1, setBush_1] = useState<boolean>(false);
+  const [bush_2, setBush_2] = useState<boolean>(false);
+  const [bush_3, setBush_3] = useState<boolean>(false);
   const trees = [];
   const bushes = [];
   const fences_verticales = [];
   const fences_horizontales = [];
   const lac_verticales = [];
+
+  const getRandomDelay = useCallback(
+    (max: number) => Math.floor(Math.random() * max),
+    [],
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const result = getRandomDelay(15);
+      if (result === 5) {
+        setBush_1(true);
+      } else {
+        setBush_1(false);
+      }
+      if (result === 10) {
+        setBush_2(true);
+      } else {
+        setBush_2(false);
+      }
+      if (result === 15) {
+        setBush_3(true);
+      } else {
+        setBush_3(false);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [getRandomDelay]);
 
   for (let i = 1; i <= 16; i++) {
     trees.push(
@@ -75,6 +106,47 @@ export default function DeskFooter() {
     <footer className={styles.footer}>
       {trees}
       {bushes}
+      {bush_1 ? (
+        <img
+          key="bush_5"
+          className={`${styles.bush_5} ${styles.animationBush}`}
+          src={bush}
+          alt="buisson"
+        />
+      ) : (
+        <img key="bush_5" className={styles.bush_5} src={bush} alt="buisson" />
+      )}
+      {bush_2 ? (
+        <img
+          key="bush_16"
+          className={`${styles.bush_16} ${styles.animationBush}`}
+          src={bush}
+          alt="buisson"
+        />
+      ) : (
+        <img
+          key="bush_16"
+          className={styles.bush_16}
+          src={bush}
+          alt="buisson"
+        />
+      )}
+      {bush_3 ? (
+        <img
+          key="bush_20"
+          className={`${styles.bush_20} ${styles.animationBush}`}
+          src={bush}
+          alt="buisson"
+        />
+      ) : (
+        <img
+          key="bush_20"
+          className={styles.bush_20}
+          src={bush}
+          alt="buisson"
+        />
+      )}
+
       {fences_verticales}
       {fences_horizontales}
       {lac_verticales}
